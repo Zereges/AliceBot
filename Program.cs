076cb82d62a9eb1e612cf10c0a52ca86690d8cd7
Zereges <zereges@ekirei.cz>
2021-02-12 13:36:23 +0100

    External configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using AliceBot.Commands;
 
 namespace AliceBot
@@ -7,13 +8,33 @@ namespace AliceBot
     {
         public static void Main(string[] args)
         {
-            var aliceTask = Alice.CreateAsync("server.com", "user", "pass", "AliceBot");
+            string server, username, password, room;
+            try
+            {
+                if (args.Length != 1)
+                {
+                    Console.WriteLine($"{System.Reflection.Assembly.GetExecutingAssembly().FullName} alice.conf");
+                    return;
+                }
+                using StreamReader reader = new StreamReader(args[0]);
+                server = reader.ReadLine();
+                username = reader.ReadLine();
+                password = reader.ReadLine();
+                room = reader.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            var aliceTask = Alice.CreateAsync(server, username, password, "AliceBot");
             using var alice = aliceTask.Result;
 
             alice.RegisterCommand(new SeenCommand(alice));
             alice.RegisterCommand(new UptimeCommand(alice));
 
-            alice.JoinRoom("group@conference.server.com");
+            alice.JoinRoom("testbot@conference.server.com");
 
             while (Console.ReadLine() != null)
                 ; // loop
